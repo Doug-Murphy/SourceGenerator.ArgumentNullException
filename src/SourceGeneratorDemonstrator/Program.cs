360653180f889974af542a89873dfdead3d456d9
Doug Murphy <doug.murphy82@gmail.com>
2021-10-12 13:12:33 -0400

    Create console app for demo/debug purposes.

---
+++ src/SourceGeneratorDemonstrator/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SourceGeneratorDemonstrator {
+	internal static class Program {
+		private static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
