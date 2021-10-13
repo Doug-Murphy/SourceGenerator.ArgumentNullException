5e652340f64af7dc2418bbfeb3119b8c9ccb6624
Doug Murphy <doug.murphy82@gmail.com>
2021-10-12 21:07:03 -0400

    Create sample program for testing and demonstration purposes.

--- src/SourceGeneratorDemonstrator/Program.cs
+++ src/SourceGeneratorDemonstrator/Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using DougMurphy.SourceGenerators.ArgumentNullException.Attributes;
+using System;
 
 namespace SourceGeneratorDemonstrator {
 	internal static class Program {
 		private static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			DemonstrateArgumentNullExceptionSourceGeneration("Foo", "Bar");
+			DemonstrateArgumentNullExceptionSourceGeneration(null, null);
+		}
+
+		private static void DemonstrateArgumentNullExceptionSourceGeneration([GenerateThrowException] string parameter1, string parameter2) {
+			Console.WriteLine($"The value specified for {nameof(parameter1)} is {parameter1} and {nameof(parameter2)} is {parameter2}");
 		}
 	}
 }
