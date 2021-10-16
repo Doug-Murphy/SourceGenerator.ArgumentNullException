4a7975a2f99f8a37708d50b4f9e2ee6cf75254b6
Doug Murphy <doug.murphy82@gmail.com>
2021-10-15 22:01:16 -0400

    Delete actual .cs file for GenerateThrowExceptionAttribute.cs in favor of building it in the generator file.

--- src/DougMurphy.SourceGenerators.ArgumentNullException/Attributes/GenerateThrowExceptionAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DougMurphy.SourceGenerators.ArgumentNullException.Attributes {
-	/// <summary>This attribute indicates to the source generator that the value of the parameter this attribute is placed on should be checked for null, and an ArgumentNullException thrown if so.</summary>
-	[AttributeUsage(AttributeTargets.Parameter)]
-	public class GenerateThrowExceptionAttribute : Attribute {
-	}
-}
